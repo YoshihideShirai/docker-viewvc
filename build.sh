0e2db8aca13e0a3655ffdb0441126eb52a452b85
Yoshihide Shirai <yoshihide.shirai@gmail.com>
2022-03-12 17:39:26 +0900

    fixed build error 1.2.1

--- build.sh
+++ build.sh
@@ -2,10 +2,11 @@
 
 VIEWVC_VERSION="1.2.1"
 
-#docker build -t yoshihide1981/viewvc:${VIEWVC_VERSION} \
-    #--build-arg VIEWVC_REFS=tags \
-    #--build-arg VIEWVC_VERSION=${VIEWVC_VERSION} \
-    #.
+docker build -t yoshihide1981/viewvc:${VIEWVC_VERSION} \
+    --file Dockerfile_buster-python2 \
+    --build-arg VIEWVC_REFS=tags \
+    --build-arg VIEWVC_VERSION=${VIEWVC_VERSION} \
+    .
 docker build -t yoshihide1981/viewvc:latest \
     --build-arg VIEWVC_REFS=heads \
     --build-arg VIEWVC_VERSION=master \
